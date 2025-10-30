1c4fd63244b038bf620e8abc2d549cf0801fe6ca
Giovvani <giovvani.m.fraga@gmail.com>
2025-10-30 00:21:18 -0600

    Agregar archivos de proyecto.

---
+++ Primero1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Primero1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1.Hola giovvas");
+            Console.WriteLine("2.Hola giovvas");
+
+
+
+            int numero = 5;
+            Console.WriteLine(numero);
+
+            int numeroLibros = 100, librosNiños = 300, librosMatematicas = 200;
+            Console.WriteLine();
+
+            bool x = false;
+            Console.WriteLine(x);
+
+            Console.WriteLine("El monto es {0} y {1}", numero,numeroLibros);
+
+        }
+    }
+}
